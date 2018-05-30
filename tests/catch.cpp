2ed09395db31f1970c5ccf1f781c314cc204bfff
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-05-30 22:40:00 +1000

    Add tests barebone

---
+++ tests/catch.cpp
@@ -0,0 +1,6 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#define CATCH_CONFIG_FAST_COMPILE               // Sacrifices some (rather minor) features for compilation speed
+#define CATCH_CONFIG_DISABLE_MATCHERS           // Do not compile Matchers in this compilation unit
+
+#include "catch.hpp"
---
+++ tests/tests.cpp
@@ -0,0 +1,7 @@
+#include "catch.hpp"
+
+TEST_CASE("Hello world")
+{
+    REQUIRE(true);
+//    REQUIRE(false);
+}
